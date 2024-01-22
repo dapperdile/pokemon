ba4693a26ddb72ebc9c8b026e72c9dd83fdecbc3
Janio-Almeida <janiovenoom@gmail.com>
2024-01-21 22:19:49 -0300

    adicao tipagem

--- main.py
+++ main.py
@@ -7,11 +7,28 @@ __version__ = open("version").readline()
 file = open('pokemon.csv')
 csvreader = csv.reader(file)
 
+file = open('Tipagem.csv')
+csvtipagem = csv.reader(file)
+
+print(csvtipagem)
+
+types_matchup = []
+
+for row in csvtipagem:
+    types_matchup.append(row)
+
+
+
 rows = []
 for row in csvreader:
     rows.append(row[1:4])
+    
 
 rows = rows[1:]
 
-types_matchup = ["Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bug", "Ghost", "Steel", "Fire", "Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy"]
+types = ["Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bug", "Ghost", "Steel", "Fire", "Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy"]
+
+pokemon1 = rows[0]
+pokemon2 = rows[6]
+print(pokemon1,' vs ', pokemon2)
 
