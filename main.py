94842c8b1f7341e8b2230e4f3d19d49183ab3fef
dapperdile <rafinhawc@hotmail.com>
2024-01-21 22:52:05 -0300

    Adicionado versão inicial de multiplicador de tipos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 
-__author__ = "diogo.shiroto"
+__author__ = "rafael.vasconcelos"
 __date__ = "21/01/2024"
 __version__ = open("version").readline()
 
@@ -10,7 +10,6 @@ csvreader = csv.reader(file)
 file = open('Tipagem.csv')
 csvtipagem = csv.reader(file)
 
-print(csvtipagem)
 
 types_matchup = []
 
@@ -31,4 +30,27 @@ types = ["Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bug", "Gho
 pokemon1 = rows[0]
 pokemon2 = rows[6]
 print(pokemon1,' vs ', pokemon2)
+
+attack = "Flying"
+defense = pokemon1[1]
+defense1 = pokemon1[2]
+
+print(defense, defense1)
+
+index_attack = None
+index_defense = None
+index_defense1 = None
+
+for index,valor in enumerate(types):
+    if valor == attack:
+        index_attack = index
+    elif valor == defense:
+        index_defense = index
+    elif valor == defense1:
+        index_defense1 = index
+
+result = int(types_matchup[index_attack][index_defense]) * int(types_matchup[index_attack][index_defense1])
+
+
+print (result)
 
