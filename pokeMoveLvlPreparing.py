d7d5b5a8b3631f90ba09faa60dab796fd9a136e3
dapperdile <rafinhawc@hotmail.com>
2024-03-12 22:32:17 -0300

    Adicionado ferramenta pra processar lista de movimentos

--- pokeMoveLvlPreparing.py
+++ pokeMoveLvlPreparing.py
@@ -1,5 +1,9 @@
 import csv
 
+__author__ = "rafael.bernardo"
+__date__ = "12/03/2024"
+__version__ = open("version").readline()
+
 file = open('data/poke_move_lvl_raw.csv')
 csvmovelvl = csv.reader(file) 
 
@@ -7,14 +11,19 @@ pokemovelist = []
 for row in csvmovelvl:
     pokemovelist.append(row)
 
+result = []
 for row in pokemovelist:
     try:
         int(row[0])
-        isint = True
+        isint = True    
     except:
         isint = False
+
     if isint == False and row[0] not in ["Evo.", "Rem."]:
         poke_name = row[0]
-    print(poke_name)
-        
-        
+    else:
+        result.append([poke_name, row[0], row[1]])
+
+with open('data/poke_move_lvl_processed.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(result)
